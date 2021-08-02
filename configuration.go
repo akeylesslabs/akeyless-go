73a62dd85901e9ecfbe5d1261fb2933a0790753d
akeyless <github@akeyless.io>
2021-08-02 10:48:42 +0000

    Release v2.5.5

--- configuration.go
+++ configuration.go
@@ -102,7 +102,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/2.5.4/go",
+		UserAgent:        "OpenAPI-Generator/2.5.5/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
