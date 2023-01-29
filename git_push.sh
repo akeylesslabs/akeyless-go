97efb0682ba5f60d2541ac7b918bfbe9f0f3900d
tuvia-akeyless <101177744+tuvia-akeyless@users.noreply.github.com>
2023-01-29 13:19:51 +0200

    by branch

--- git_push.sh
+++ git_push.sh
@@ -7,6 +7,7 @@ git_user_id=$1
 git_repo_id=$2
 release_note=$3
 git_host=$4
+git_branch=$5
 
 if [ "$git_host" = "" ]; then
     git_host="github.com"
@@ -31,6 +32,12 @@ fi
 # Initialize the local directory as a Git repository
 git init
 
+# Create branch and checkout if not master
+if [ "$git_branch" != "master" ]; then
+    git checkout -b "$git_branch"
+    echo "[INFO] switch to branch: $git_branch"
+fi
+
 # Adds the files in the local repository and stages them for commit.
 git add .
 
@@ -50,9 +57,9 @@ if [ "$git_remote" = "" ]; then # git remote not defined
 
 fi
 
-git pull origin master
+git pull origin "$git_branch"
 
 # Pushes (Forces) the changes in the local repository up to the remote repository
 echo "Git pushing to https://${git_host}/${git_user_id}/${git_repo_id}.git"
-git push origin master 2>&1 | grep -v 'To https'
+git push origin "$git_branch" 2>&1 | grep -v 'To https'
 
